5eb0f366d86038a89d07fd19b5410f68b8c5a352
sunghyunkang <ksh901016@naver.com>
2020-12-02 21:53:06 +0900

    # init
    init

---
+++ src/lamda/example.kt
@@ -0,0 +1,19 @@
+package lamda
+
+fun printProblemCounts(responses: Collection<String>){
+    var clientErrors = 0
+    var serverErrors = 0
+    responses.forEach{
+        if(it.startsWith("4")){
+            clientErrors++
+        }else if(it.startsWith("5")){
+            serverErrors++
+        }
+    }
+    println("$clientErrors client erros, $serverErrors server errors")
+}
+
+fun main() {
+    val responses = listOf("200 OK", "418 I'm a teapot", "500 Internal Server Error")
+    printProblemCounts(responses)
+}
